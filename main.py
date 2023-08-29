9e17d14009973d27f55e7da3250e6536b1b9b698
Lais Maia <laais.fmaia@gmail.com>
2023-08-29 15:29:33 -0300

    criado função de exibição de mensagem

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ def gether_data():
 
     return n1, n2
 
+def print_message(n1,n2):
+    print(f'Os valores {n1} e {n2} somados são {n1+n2}')
+
+    return None
+
 def main():
     n1, n2 = gether_data()
+
+    return print_message(n1,n2)
     return None
 
 if __name__ == "__main__":
